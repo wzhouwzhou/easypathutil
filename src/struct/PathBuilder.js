a8134421c1ca205a55c3c879f86b0e1c832cff7e
William Zhou <wzhouwzhou@gmail.com>
2018-10-31 22:29:00 -0400

    Reuse path module, make use of path module for joins, and introduce $readdir(sync) traps

--- src/struct/PathBuilder.js
+++ src/struct/PathBuilder.js
@@ -3,13 +3,14 @@ Reflect.defineProperty(exports, '__esModule', { value: true });
 
 const { traps: { has, get } } = require('../deps');
 
-function PathBuilder(base = process.cwd(), parts = []) {
+function PathBuilder(base = process.cwd(), parts = [], path = require('path')) {
   if (!(this instanceof PathBuilder)) return new PathBuilder(base);
   this.base = base;
   this.parts = parts;
+  this._path = path;
 
   const proxy = this.proxy = new Proxy((arga => {
-    if (!arga) return `${this.base}/${this.parts.join('/')}`.replace(/\/$/, '');
+    if (!arga) return this._path.join(this.base, ...this.parts);
     return new PathBuilder(this.base, [...this.parts, arga.toString()]);
   }).bind(this), { has: has.bind(this), get: get.bind(this) });
   return proxy;
