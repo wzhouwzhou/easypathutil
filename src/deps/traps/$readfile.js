8b3c2972f0580a445834666d52935af9c0120a62
William Zhou <wzhouwzhou@gmail.com>
2019-06-20 14:14:44 -0400

    Update $readfile - better error info

--- src/deps/traps/$readfile.js
+++ src/deps/traps/$readfile.js
@@ -3,11 +3,11 @@ const regex = exports.regex = /^\$read(?:[._]*)?file(?:[._]*sync)?$/i;
 exports.condition = ({ stringprop }) => regex.test(stringprop);
 exports.value = function value(object, prop, stringprop) {
   if (stringprop.toLowerCase().includes('sync')) {
-    if (!this.proxy.$statsync.file) throw new Error('Read: I am not a file.');
+    if (!this.proxy.$statsync.file) throw new Error(`Read: I am not a file. (Tried to access ${this.proxy()} unsuccessfully.)`);
     return this._fs.readFileSync(this.proxy());
   } else {
     return Promise.resolve((async() => {
-      if (!(await this.proxy.$stat).file) throw new Error('Read: I am not a file.');
+      if (!(await this.proxy.$stat).file) throw new Error(`Read: I am not a file. (Tried to access ${this.proxy()} unsuccessfully.)`);
       return new this._Promise((res, rej) => this._fs.readFile(this.proxy(), (err, data) => {
         if (err) return rej(err);
         return res(data);
