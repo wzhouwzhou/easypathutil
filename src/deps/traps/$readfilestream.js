d6dcc76b25c91a70494c144834dc5fc3c49cde09
William Zhou <wzhouwzhou@gmail.com>
2019-06-20 14:15:37 -0400

    Update $readfilestream - Better error message

--- src/deps/traps/$readfilestream.js
+++ src/deps/traps/$readfilestream.js
@@ -2,6 +2,6 @@ const regex = exports.regex = /^\$read(?:[._]*)?file(?:[._]*)?stream$/i;
 
 exports.condition = ({ stringprop }) => regex.test(stringprop);
 exports.value = function value() {
-  if (!this.proxy.$statsync.file) throw new Error('Read: I am not a file.');
+  if (!this.proxy.$statsync.file) throw new Error(`Read: I am not a file. (Tried to access ${this.proxy()} unsuccessfully.)`);
   return this._fs.createReadStream(this.proxy(), this.readFileStreamOptions || {});
 };
